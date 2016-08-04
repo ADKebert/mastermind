49c6146c58f9939754b6fb9ec8bf92774a151c58
Alan Kebert <adkebert@yahoo.com>
2016-08-04 16:24:44 -0400

    success getting sinatra to auto reload

--- web.rb
+++ web.rb
@@ -1,9 +1,11 @@
 require "sinatra"
+require "sinatra/reloader" if development?
 
 require_relative "mastermind"
 
 $mastermind_cpu = MasterMind.new
 get "/" do
+  puts "testing"
   @guesses = $mastermind_cpu.guess_history
   haml :gameboard
 end
