78fc4f64396e24577dde03a78c2a16fd2e57ddd6
Alan Kebert <adkebert@yahoo.com>
2016-08-04 21:02:57 -0400

    added logic for winning and losing and made play again buttons reset the game

--- web.rb
+++ web.rb
@@ -7,15 +7,23 @@ $mastermind_cpu = MasterMind.new
 
 get "/" do
   @guesses = $mastermind_cpu.guess_history
+  @results = $mastermind_cpu.result_history
   haml :gameboard
 end
 
 post "/" do
-  $mastermind_cpu.guess [params["Peg1"],
-                         params["Peg2"],
-                         params["Peg3"],
-                         params["Peg4"]]
-  redirect "/"
+  guess = [params["Peg1"],
+           params["Peg2"],
+           params["Peg3"],
+           params["Peg4"]]
+  $mastermind_cpu.guess(guess)
+  if $mastermind_cpu.result_win?(guess)
+    redirect "/win"
+  elsif $mastermind_cpu.guess_count >= 10
+    redirect "/lose"
+  else
+    redirect "/"
+  end
 end
 
 get "/win" do
